330017fa585a62f12b48db20acd3931833c3fb8e
taehyunhwang72 <taehyunhwang72@gmail.com>
2018-08-26 18:38:32 +0900

    speech.js added

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,9 @@ import 'popper.js';
 import 'bootstrap';
 import 'lightbox2/dist/js/lightbox-plus-jquery.js';
 
+import { tooltip, popover } from './modules/bsComponents.js';
 import { wow } from './modules/wowAnimate.js';
 import { shadowRaise, shadowDown } from './modules/shadow.js';
 import scroll from './modules/scroll.js';
-import { tooltip, popover } from './modules/bsComponents.js';
 import { toggleMenu } from './modules/toggleMenu.js';
+import speech from './modules/speech.js';
---
+++ src/js/modules/speech.js
@@ -0,0 +1,41 @@
+/**
+ * https://tutorialzine.com/2017/08/converting-from-speech-to-text-with-javascript
+ */
+
+import $ from 'jquery';
+
+var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+var noteTextarea = $('#search-term');
+var noteContent = '';
+
+recognition.continuous = true;
+
+recognition.onresult = function (e) {
+    var current = e.resultIndex;
+    var transcript = e.results[current][0].transcript;
+    var mobileRepeatBug = (current == 1 && transcript == e.results[0][0].transcript);
+
+    if (!mobileRepeatBug) {
+        noteContent += transcript;
+        noteTextarea.val(noteContent);
+    }
+};
+
+$('#start-record-btn').on('click', function (e) {
+    if (noteContent.length) {
+        noteContent += ' ';
+    }
+    recognition.start();
+});
+
+$('#pause-record-btn').on('click', function (e) {
+    recognition.stop();
+});
+
+noteTextarea.on('input', function () {
+    noteContent = $(this).val();
+})
+
+export default speech
