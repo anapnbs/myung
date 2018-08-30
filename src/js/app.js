9ae53e0085fbb154408cb7818a00e8c8eb40a290
taehyunhwang72 <taehyunhwang72@gmail.com>
2018-08-30 16:55:58 +0900

    toggle Nav remove

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,7 @@ import 'lightbox2/dist/js/lightbox-plus-jquery.js';
 
 import { tooltip, popover } from './modules/bsComponents.js';
 import { wow } from './modules/wowAnimate.js';
-import { imgClick } from './modules/gallery.js';
 import { shadowRaise, shadowDown } from './modules/shadow.js';
 import scroll from './modules/scroll.js';
-import { toggleMenu } from './modules/toggleMenu.js';
 import speech from './modules/speech.js';
+import { imgClick } from './modules/gallery.js';
